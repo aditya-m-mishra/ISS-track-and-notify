e13cedf7d3a6ee5646dc5837a1afbee8cbeab1ac
aditya-m-mishra <adiapplies@gmail.com>
2026-05-22 18:08:37 +0530

    check for night func added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
@@ -24,4 +25,22 @@ def is_iss_overhead():
         MY_LAT - 5 <= iss_latitude <= MY_LAT + 5
         and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5
     ):
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+    if time_now >= sunset or time_now <= sunrise:
         return True
